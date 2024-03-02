93c3bbe8941cbad345f459a800a01c7c7e5a9cd0
Rave Flores <reyvflores40@gmail.com>
2024-03-02 20:16:03 +0800

    Version 0.5.2

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const dateEdition = new Date();
 document.querySelector("#version-number").innerHTML = `${dateEdition.getFullYear()} Edition`;
 
-// alert("Responsive design is under development.");
-alert("I love you Preccia");
+alert("Responsive design is under development. Best viewed in desktop.");
